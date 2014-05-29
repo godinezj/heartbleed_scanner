3306f79f6d39ef08e14d3e9eeb80ea88c4c3a5ef
Javier Godinez <Javier_Godinez@intuit.com>
2014-05-29 12:14:44 -0700

    added filtering to results generation

--- process.sh
+++ process.sh
@@ -3,7 +3,7 @@
 function do_work {
   ruby get_job.rb > input.txt
   ruby msf_scanner.rb input.txt > output.txt
-  grep "[+]" output.txt > results.txt
+  grep "\[+\]" output.txt|tr -cd '\11\12\15\40-\176'|cut -d\  -f1 --complement  > results.txt
   ruby send_results.rb results.txt
   rm results.txt output.txt input.txt
 }
