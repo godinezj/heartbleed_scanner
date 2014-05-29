6e500c6de213590cddc340c88c3f62400c95a823
Javier Godinez <Javier_Godinez@intuit.com>
2014-05-29 08:58:23 -0700

    polling both queues for results

--- get_results.rb
+++ get_results.rb
@@ -7,20 +7,24 @@ AWS.config(:region => ENV['AWS_REGION'],
            :credential_provider => AWS::Core::CredentialProviders::EC2Provider.new)
 sqs = AWS::SQS.new()
 res_url = ENV['RESPONSE_QUEUE_URL']
-req_uel = ENV['REQUEST_QUEUE_URL']
+req_url = ENV['REQUEST_QUEUE_URL']
 res_q = sqs.queues[res_url]
 req_q = sqs.queues[req_url]
 
-while req_q.visible_messages > 0 and res_q.visible_messages > 0 do
-  res_q.receive_message(:wait_time_seconds => 60) { |msg|
+puts "Request queue has #{req_q.visible_messages} messages, Response queue has #{res_q.visible_messages} messages"
+while req_q.visible_messages > 0 || res_q.visible_messages > 0 do
+  puts "Request queue has #{req_q.visible_messages} messages, Response queue has #{res_q.visible_messages} messages"
+  res_q.receive_message(:wait_time_seconds => 20) { |msg|
     puts msg.body
   }
   sleep 60
 end
 
 sleep 120
-res_q.receive_message(:wait_time_seconds => 60) { |msg|
-  puts msg.body
-}
+if req_q.visible_messages > 0
+  res_q.receive_message(:wait_time_seconds => 20) { |msg|
+    puts msg.body
+  }
+end
 
 puts "Done!"
