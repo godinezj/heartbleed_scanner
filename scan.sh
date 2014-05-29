6a36ce9ee707b4190405dec50e58bdd997293c7e
Javier Godinez <Javier_Godinez@intuit.com>
2014-05-29 12:51:12 -0700

    Added lock file and now calling get_results.rb

--- scan.sh
+++ scan.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-nmap -n -PN -p443 -sT --randomize-hosts -oG results.txt \
+
+# make sure only one job runs at the time
+touch running.lock
+
+# scan the ranges or ip addresses in all-ranges.txt
+nmap -n -PN -p443 -sT --randomize-hosts -oG scan-results.txt -T5 \
   --open -iL all-ranges.txt --excludefile exclude-hosts.txt
-grep -v \# results.txt | grep open/tcp |cut -f2 -d\  > live-hosts.txt
+
+# parse the results
+grep -v \# scan-results.txt | grep open/tcp |cut -f2 -d\  > live-hosts.txt
+
+# queue up the jobs and retrieve results
 ruby add_jobs.rb live-hosts.txt
+ruby get_results.rb > results.txt
 
-
-
+rm -f running.lock
