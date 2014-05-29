596542b0d1a7867c279f0e507aab1bcb4571a466
Javier Godinez <Javier_Godinez@intuit.com>
2014-05-29 13:25:08 -0700

    now excluding duplicate results

--- process.sh
+++ process.sh
@@ -3,9 +3,9 @@
 function do_work {
   ruby get_job.rb > input.txt
   ruby msf_scanner.rb input.txt > output.txt
-  grep "\[+\]" output.txt|tr -cd '\11\12\15\40-\176'|cut -d\  -f1 --complement  > results.txt
+  grep "\[+\]" output.txt|tr -cd '\11\12\15\40-\176'|cut -d\  -f1 --complement|sort|uniq  > results.txt
   ruby send_results.rb results.txt
-  rm results.txt output.txt input.txt
+  rm -f results.txt output.txt input.txt
 }
 
 while true; do 
